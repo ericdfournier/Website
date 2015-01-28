5b893cf4b94bc4c0fb0bebba729646f2492fdc10
Eric Fournier <me@ericdfournier.com>
2015-01-28 06:57:49 -0800

    Updated to version 5.0. Simpler layout. More text based. Not designed like I am some kind of startup company trying to sell you something.

---
+++ versions/4.0/main.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+  "log"
+  "net/http"
+)
+
+func main() {
+  fs := http.FileServer(http.Dir("static"))
+  http.Handle("/static/", http.StripPrefix("/static/", fs))
+
+  log.Println("Listening...")
+  http.ListenAndServe(":3000", nil)
+}
